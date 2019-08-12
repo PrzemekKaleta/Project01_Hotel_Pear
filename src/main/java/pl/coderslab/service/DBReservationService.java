5e819addf766d2e667a93769f349e3310b7a33a1
Przemek Kaleta <przemyslawkaleta@gmail.com>
2019-08-12 21:01:33 +0200

    logic change at DBReservationService

--- src/main/java/pl/coderslab/service/DBReservationService.java
+++ src/main/java/pl/coderslab/service/DBReservationService.java
@@ -57,31 +57,63 @@ public class DBReservationService extends ReservationService {
     @Override
     public boolean findRoom(ReserveAsk reserveAsk) {
 
+        //zapisujemy stałe dane: pojemność pokojów które będą przeglądane, liczby reprezentujące zapytanie od kiedy do kiedy ma być szukane miejsce
         int roomCapacity = reserveAsk.getPersons();
         long askFromNumber = reserveAsk.getDateFrom().toEpochDay();
         long askUntilNumber = reserveAsk.getDateUntil().toEpochDay();
 
+        //definiujemy listę do której będziemy zapisywać id pokojów które będa spełniać warunki oraz stopień dopasowania (czym niższa wartość tym lepiej)
         Map<Long, Double> fitMap = new HashMap<>();
 
+        //defiujemy listę pokojów które spełniły warunek pojemności
         List<Room>rooms = roomRepository.findAllByCapacity(roomCapacity);
 
+        //i-terójemy po liście szukając tych odpowiednich
         for(int i = 0 ; i < rooms.size() ; i++){
 
+            //w pierwszej kolejności tworzymy listę pobytów które albo są aktualne albo są zarezerwowane (tj są aktywne, aby nie ściągać historycznych danych)
             List<Stay>stays = stayRepository.findAllByStayStatesAndRoomId(StayState.CURRENT, StayState.RESERVED, rooms.get(i).getId());
 
+            //jeżeli lista jest pusta to oznacza że nie ma jeszcze aktywnych pobytów i odrazu możemy zapisać pokój do MAPY fitMap, nadając liczbę fit 10
             if(stays.isEmpty()){
 
                 fitMap.put(rooms.get(i).getId(), 10.0);
 
             }else{
 
+                //jeżeli lista nie była pusta to tworzymy dwie wartości będące składową późniejszej liczby FIT
+                long holeBefore = 10;
+                long holeAfter = 10;
+
+                //j-terójemy po liście pobytów dla danego pokoju
                 for(int j = 0 ; j < stays.size() ; j ++){
 
+                    //zapisujemy wartości w postaci liczbowej dla dnia rozpoczęcia i zakończenia pobytu, do którego będziemy dopasowywać zapytanie
                     long stayFromNumber = stays.get(j).getStayFrom().toEpochDay();
-                    long stayUntilNumeber = stays.get(j).getStayUntil().toEpochDay();
+                    long stayUntilNumber = stays.get(j).getStayUntil().toEpochDay();
 
-                    
+                    //tworzymy dwie liczby będące różnicą pomiędzy zakończeniem pobytu i rozpoczęciem zapytania oraz zakończeniem zapytania i rozpoczeciem pobytu
+                    long differenceAskFromToStayUntil = askFromNumber - stayUntilNumber;
+                    long differenceStayFromToAskUntil = stayFromNumber - askUntilNumber;
 
+                    System.out.println("różnica: " + differenceAskFromToStayUntil + " pomiędzy AskFrom " + askFromNumber + ", a StayUntil " + stayUntilNumber);
+                    System.out.println("różnica: " + differenceStayFromToAskUntil + " pomiędzy StayFrom " + stayFromNumber + ", a AskUntil " + askUntilNumber);
+
+                    //sprawdzenie warunków obie liczby "różnice" powinny mieć inny znak
+                    if((differenceAskFromToStayUntil >= 0 && differenceStayFromToAskUntil <= 0)||(differenceAskFromToStayUntil <=0 && differenceStayFromToAskUntil >= 0){
+
+                        //sprawdzamy czy różnica przed jest dodatnia lub zerowa, jeżeli tak to czy jest też mniejsza od dotychczasowej wartości bo...
+                        //...jeżeli będziemy i-terować to kolejne pobyty mogą być bliżej najszego zapytania a wiec stopień dopasowania będzie lepszy...
+                        //...jeżeli tego nie dodamy tego warunku to możemy nadpisać lepsze dopasowanie gorszym
+                        if(differenceAskFromToStayUntil>=0 && differenceAskFromToStayUntil < holeBefore){
+                            holeBefore = differenceAskFromToStayUntil;
+                            }
+                            // podobnie jak poprzednio sprawdzamy dopasowanie po (tutaj pierwszy warunek powinien być już spełniony z automatu - jeżeli wykożysta się 'else if'
+                         if(differenceStayFromToAskUntil >= 0 && differenceStayFromToAskUntil < holeAfter){
+                             holeAfter = differenceStayFromToAskUntil;
+                         }
+
+                    }
 
 
 
